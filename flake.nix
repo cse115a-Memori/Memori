c08bcec5318ef301519b30a0c44e3ecf17c6bd91
suri-codes <suri312006@gmail.com>
2026-01-17 11:13:55 -0800

    example simulator
    basic simulator

--- flake.nix
+++ flake.nix
@@ -1,11 +1,8 @@
 {
   description = "A Nix-flake-based Typst development environment";
-
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
-
   outputs =
     { self, ... }@inputs:
-
     let
       supportedSystems = [
         "x86_64-linux"
@@ -35,10 +32,26 @@
                 typst
                 typstyle
                 tinymist
+                # for the simulator
+                SDL2
+                pkg-config
+                libiconv
               ]
               ++ (with typstPackages; [
                 # Typst packages
               ]);
+            shellHook = ''
+              export LIBRARY_PATH=${
+                pkgs.lib.makeLibraryPath [
+                  pkgs.SDL2
+                  pkgs.libiconv
+                ]
+              }:$LIBRARY_PATH
+              export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.SDL2 ]}:$LD_LIBRARY_PATH
+              ${pkgs.lib.optionalString pkgs.stdenv.isDarwin ''
+                export DYLD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [ pkgs.SDL2 ]}:$DYLD_LIBRARY_PATH
+              ''}
+            '';
           };
         }
       );
--- justfile
+++ justfile
@@ -1,10 +1,8 @@
-
-dev LOG="debug":
-    cd ./memori-dev && ESP_LOG={{ LOG }} cargo run --release
+esp LOG="debug":
+    cd ./memori-dev/esp32c3/ && ESP_LOG={{ LOG }} cargo run --release
 
 ios:
     cd ./memori-app && bun tauri ios dev "iPhone 17 Pro"
 
 typ FILE="":
     typst watch {{ FILE }}
-
