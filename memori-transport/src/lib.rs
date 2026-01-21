e90feedfd26c9a6b1adc6ce88f73211d31d2819a
suri-codes <suri312006@gmail.com>
2026-01-21 09:19:07 -0800

    Added basic types for transport

--- memori-transport/src/lib.rs
+++ memori-transport/src/lib.rs
@@ -1,14 +1,82 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use serde::Deserialize;
+use serde::Serialize;
+
+pub type ByteArray = heapless::Vec<u8, 1024>;
+
+#[derive(Serialize, Deserialize)]
+pub struct WidgetId(u32);
+
+
+#[derive(Serialize, Deserialize)]
+enum MemError {
+    NoAck,
+    WidgetNotFound,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+type MemResult<T> = Result<T, MemError>
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+
+
+#[derive(Serialize, Deserialize)]
+enum Javelin {
+    // inital setup
+    SetWidgets(Widget),
+
+    // get currently flashed widgets
+    GetWidget(WidgetId),
+
+    // respond to prods with updated data
+    Data((WidgetId, ByteArray)),
+
+    // Battery Level
+    GetBatteryLevel,
+
+    /// Set device configuration
+    SetConfig(DeviceConfig),
 }
+
+#[derive(Serialize, Deserialize)]
+enum Needle {
+    SendWidget(Widget),
+    Prod(WidgetId),
+
+}
+
+
+// ble {Javelin::SetWidget} -> BLE -> Device -> BLE (ack) -> btleplug -> Ok(())
+#[derive(Serialize, Deserialize)]
+struct Widget {
+    id: u8,
+    data: ByteArray,
+}
+
+#[derive(Serialize, Deserialize)]
+struct DeviceConfig {
+    dark_mode: bool,
+}
+// trait MemTransport {
+//     pub fn send_javelin(javelin: Javelin) -> MemResult<Option<Needle>> ;
+//     pub fn send_needle(needle: Needle) -> MemResult<Option<Javelin>> ;
+// }
+
+// struct MemBLEtransport {
+
+// }
+
+// impl MemTransport for MemBLEtransport {
+
+//     pub fn send_javelin(javelin: Javelin) -> MemResult<Option<Needle>> {
+//         todo!()
+//     }
+
+//     pub fn send_needle(needle: Needle) -> MemResult<Option<Javelin>> {
+//         todo!()
+//     }
+
+// }
+
+
+
+// struct MemoriTransport<T: MemTransport> {
+//     transport: T,
+// }
